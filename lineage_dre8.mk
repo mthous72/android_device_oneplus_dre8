d65c10117951e910320d4cb28a74d2d7155dc664
mthous72 <mthous01@hotmail.com>
2022-01-27 12:01:18 -0500

    Update and rename lineage_renoir.mk to lineage_dre8.mk
    Update to oneplus and dre8

--- lineage_dre8.mk
+++ lineage_dre8.mk
@@ -8,16 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from renoir device
+# Inherit from dre8 device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_BRAND := Xiaomi
-PRODUCT_DEVICE := renoir
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := lineage_renoir
+PRODUCT_BRAND := OnePlus
+PRODUCT_DEVICE := dre8
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := Nord N200 5g
+PRODUCT_NAME := lineage_dre8
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
